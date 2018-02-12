0c7e388c328494f02462b97a951d438dea97d122
shaunaono <shaunaono@gmail.com>
2018-02-11 19:08:10 -0800

    first push

---
+++ assets/javascript/game.js
@@ -0,0 +1,19 @@
+// Four crystals- randomly valued between 1-12
+//values reset every new game.
+$(".cup-image").on("click", function(){
+	alert("beans");
+});
+
+// Random # to acheive between 19-120; to reset
+// with every new game.
+
+// On click to find out crystals value during game
+
+// Score counter to keep score during game
+
+// Wins counter
+
+// Loses counter
+
+// Restart game
+
